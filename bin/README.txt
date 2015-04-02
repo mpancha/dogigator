67585bc3fb87792be86a4f9673b2c02f76f870e9
Mitul Panchal <mpancha@ncsu.edu>
2015-04-02 01:57:49 -0400

    Built in LED drive python code

--- bin/README.txt
+++ bin/README.txt
@@ -1,2 +1,6 @@
 # dogigator
 Wireless Project code (ECE575)
+
+indications.py - Python code to drive built in LEDs on BBB
+
+The direction is indicated by blinking the corresponding LED
---
+++ bin/indications.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import time
+left='/sys/class/leds/beaglebone:green:usr0/brightness'
+right='/sys/class/leds/beaglebone:green:usr1/brightness'
+front='/sys/class/leds/beaglebone:green:usr2/brightness'
+back='/sys/class/leds/beaglebone:green:usr3/brightness'
+
+def turn(direction):
+	f=open(direction,'w')
+	f.write("255")
+	f.close()
+	time.sleep(5)
+	f=open(direction,'w')
+	f.write("0")
+	f.close()
+turn(left)
+turn(right)
+turn(front)
+turn(back)
+
